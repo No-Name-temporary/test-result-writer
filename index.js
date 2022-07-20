180c42e424677c893a09d701e8479bdc8f31078e
Tim Dronkers <tdronkers@gmail.com>
2022-07-19 21:07:58 -0600

    adding lambda client SDK for invocation of test-alerts lambda
    Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com>
    Co-authored-by: Miles Abbason <miles.abbason@gmail.com>
    Co-authored-by: Scott Graham <scttgrhm7+public@gmail.com>

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ const {
   insertAssertionResultData,
 } = require('./lib/db_query');
 require('dotenv').config();
+const { failedTestScreener } = require('./lib/failedTestScreener');
+const { invokeTestAlerts } = require('./lib/aws/lambdaActions');
 
 exports.handler = async (event) => {
   const sqsMessage = new SqsMessage(event);
   console.log('sqsMessage.message -->', sqsMessage.message);
   const testResult = new TestResult(sqsMessage.message);
-  
+
+  console.log('Failed Assertions Present? ', failedTestScreener(sqsMessage.message.results));
+  if (failedTestScreener(sqsMessage.message.results)) {
+    try {
+      invokeTestAlerts(sqsMessage.message);
+    } catch (e) {
+      console.error(`invocation of test-alerts lambda threw an error: ${e.message}. Continuing with DB operation...`);
+    }
+  }
+
   // TODO: move to class(es) and optimize to use way fewer database operations
   const testMetadata = await getTestByName({ testName: testResult.testName });
   const regionMetadata = await getRegionByAWSName({ awsRegionName: sqsMessage.awsRegion });
